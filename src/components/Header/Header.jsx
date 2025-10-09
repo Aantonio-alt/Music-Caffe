58cf7ebe437f13a5bb109c018fd2e100013fecd5
Aantonio-alt <aantoniodevalt@gmail.com>
2025-10-09 13:41:22 -0600

    cambios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom"
-
+import "./index.css"
 const Header = () => {
     return(
         <header className='header'>
             <h1>Music Caffe </h1>
 
             <form className="formulario">
-                <label> Buscar:
-                    <input type="search" placeholder="Buscar Artista"/>
-                    <button type="submit"><i class="bi bi-search"></i></button>
-                </ label>
+
+                    <input className="buscador" type="search" placeholder="Buscar Artista"/>
+                    <button className="butonSearch" type="submit"><i class="bi bi-search"></i></button>
+  
             </form>
 
             <nav className="menuOptions">
-                <Link to="/home" >Inicio</Link> 
-                <Link to="/detailSong">Detalles</Link>
+                <Link className="enlace" to="/home" >Inicio</Link> 
+                <Link className="enlace" to="/detailSong">Detalles</Link>
             </nav>
         </header>        
     )
