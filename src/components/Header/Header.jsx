6cbbd236b19d8c2a3ccccf775bd755abba5bb273
Aantonio-alt <aantoniodevalt@gmail.com>
2025-10-08 19:32:33 -0600

    a

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ const Header = () => {
     return(
         <header className='header'>
             <h1>Music Caffe </h1>
-            <nav>
+
+            <form className="formulario">
+                <label> Buscar:
+                    <input type="search" placeholder="Buscar Artista"/>
+                    <button type="submit"><i class="bi bi-search"></i></button>
+                </ label>
+            </form>
+
+            <nav className="menuOptions">
                 <Link to="/home" >Inicio</Link> 
                 <Link to="/detailSong">Detalles</Link>
             </nav>
